4d46c5fcad742c80b8dbe0a7b478c68b81b8b7f1
阿部直也 <n.abe@gemcook.com>
2022-12-30 20:57:28 +0900

    swagger-uiの追加

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,16 @@
 import express, { Express, Request, Response } from 'express';
+import swaggerUi from 'swagger-ui-express';
+import yaml from 'js-yaml';
+import * as fs from 'fs';
+import path from 'path';
 
 const app: Express = express();
 app.use(express.json());
+
+const swaggerPath = path.resolve(__dirname, '../docs/swagger.yaml');
+const swaggerDocument = yaml.load(fs.readFileSync(swaggerPath, 'utf-8'));
+// @ts-ignore
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)); // Todo:後々、型定義について修正する
 
 app.get('/', (req: Request, res: Response) => {
 	res.status(200).json({ message: 'ok' });
