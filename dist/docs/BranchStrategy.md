f0982ba6b6f6764b03e2fb6a3617b9bcf8904ec0
阿部直也 <n.abe@gemcook.com>
2022-12-30 22:28:16 +0900

    ディレクトリ構成の変更

---
+++ dist/docs/BranchStrategy.md
@@ -0,0 +1,14 @@
+# ブランチ戦略
+
+プロト1開発では**GitLab-flow**を採用する。
+
+本来はtrunkベースでの開発が必要だが、コンセンサスベイス社のFork権限が付与できないので、プロト開発 1 では諦める。
+
+プロト開発1では以下の整理で、branchを作成する。
+
+- 本番（エンジニア以外動作確認）：main
+- 動作確認（エンジニア、プロダクトオーナー）：dev
+- 開発（アサインされたエンジニア）：feature/JIRA のチケット id_JIRA のチケット名
+  - 例：feature/BLC-9\_フロント Holder 画面の作成
+
+[参考：Git-flow 　 GitHub-flow GitLab-flow という開発フローについてまとめる](https://qiita.com/pandama09396862/items/9f013fa7b60f4d12d1d8)
--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = __importDefault(require("./app"));
+const app_1 = __importDefault(require("./src/app"));
 const port = process.env.PORT || 8080;
 app_1.default.listen(port, () => console.log(`listening on port ${port}!`));
