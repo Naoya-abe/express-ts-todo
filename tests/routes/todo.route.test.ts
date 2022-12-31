9289e89e139b462aa49031199ee861191202234e
阿部直也 <n.abe@gemcook.com>
2022-12-31 12:27:23 +0900

    DBの導入完了。Prismaの設定はこれから。

--- tests/routes/todo.route.test.ts
+++ tests/routes/todo.route.test.ts
@@ -1,1 +1,10 @@
-export const test = {};
+// import request from 'supertest';
+// import todoRouter from '../../src/routes/todo.route';
+
+// describe('test app.ts', () => {
+// 	test('test "/todo/list"', async () => {
+// 		const response = await request(todoRouter).get('/todo/list');
+// 		expect(response.statusCode).toBe(200);
+// 		expect(response.body.message).toBe('ok');
+// 	});
+// });
